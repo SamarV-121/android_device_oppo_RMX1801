427b3398d591f5a0fd99b2d940a5d974bd8a9e3d
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    RMX1801: Build RCS
    Change-Id: I456aa2db554647f4f6303964f7fb071f98b923ac

--- device.mk
+++ device.mk
@@ -42,6 +42,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
