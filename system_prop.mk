bbaa98301d70a98f0856752b64d0e6b874279b2e
Paul Keith <javelinanddart@gmail.com>
2018-03-08 23:44:00 +0530

    RMX1801: Enable QCRIL radio power saving
    Change-Id: I339d0fbec60b554c6b7930162f5a917e052fc5a1

--- system_prop.mk
+++ system_prop.mk
@@ -12,3 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.core_ctl_max_cpu=4 \
     ro.vendor.qti.core_ctl_min_cpu=2
 
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.add_power_save=1
+
