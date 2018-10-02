cf766d7247147016b65eb8fdd7446915c3cf736d
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    RMX1801: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,9 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 # BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 # BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
 # Vendor
 TARGET_COPY_OUT_VENDOR := vendor
 
