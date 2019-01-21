e27e36bc10f77c4fec5cc1225e1211044e158df6
bgcngm <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    RMX1801: Migrate to livedisplay 2.0
    * also define livedisplay HAL in framework manifest
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Misc
 PRODUCT_COPY_FILES += \
