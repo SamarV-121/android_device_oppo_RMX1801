ebf374a2f47ecce864b58e00c6b5f6f5337eb183
SamarV-121 <samarvispute121@gmail.com>
2019-03-13 03:37:11 +0000

    RMX1801: Import overlays from stock

--- device.mk
+++ device.mk
@@ -6,5 +6,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Call proprietary blob setup
 $(call inherit-product-if-exists, vendor/oppo/RMX1801/RMX1801-vendor.mk)
