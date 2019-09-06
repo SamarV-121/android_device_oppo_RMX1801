fc2d507af56241b55d2177d6621b5343a62212cc
SamarV-121 <samarvispute121@gmail.com>
2019-09-06 12:12:28 +0000

    RMX1801: Define tap to wake node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,9 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 TARGET_USERIMAGES_USE_EXT4 := true
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
 
