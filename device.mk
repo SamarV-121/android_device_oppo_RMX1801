c466764a71973c4c6860979c63ed46ce9ddb91f6
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-09-09 16:52:33 +0200

    RMX1801: Move soong namespaces to device.mk
    * This is a product property
    
    Change-Id: I411428687839fca7c794d488bb48e5f1773e570b
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -96,6 +96,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
+
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
