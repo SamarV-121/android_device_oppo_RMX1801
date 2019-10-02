cf942a85a3b4dde149f18bfdc27f3b9541268510
SamarV-121 <samarvispute121@gmail.com>
2019-10-02 20:30:08 +0530

    RMX1801: Add some system props
     * also Move them to /system/etc/prop.default
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,51 +1,62 @@
 # Assertive display
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.qcom.ad=1 \
     ro.qcom.ad.sensortype=2 \
     ro.qcom.ad.calib.data=/vendor/etc/OPPO_OLED_AD_calib.cfg
 
 # Audio
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.config.vc_call_vol_steps=7 \
    ro.config.media_vol_steps=25
 
 # Camera
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.camera.HAL3.enabled=1
 
 # CNE
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.cne.feature=1
 
-# Graphics
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.hw=1 \
-    debug.sf.latch_unsignaled=1
+# Data Modules
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=true
 
+# Graphics
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    debug.sf.hw=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.enable_hwc_vds=1	
+ 
 # HAL1 apps list
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android
 
 # IMS
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
+# Netflix
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.netflix.bsp_rev=Q660-13149-1
+	
 # Memory optimizations
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.qti.sys.fw.bservice_enable=true
 
 # Perf
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.qti.core_ctl_max_cpu=4 \
-    ro.vendor.qti.core_ctl_min_cpu=2
+    ro.vendor.qti.core_ctl_min_cpu=2 \
+    vendor.iop.enable_prefetch_ofr=0 \
+    vendor.iop.enable_uxe=0	
 
 # Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.add_power_save=1 \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.telephony.default_network=9,0
 
 # UI
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     sys.use_fifo_ui=1
