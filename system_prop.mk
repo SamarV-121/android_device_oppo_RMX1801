947d9ae935431a9c986b1e0a5da7b914df92d599
SamarV-121 <samarvispute121@gmail.com>
2020-01-08 06:04:14 +0000

    RMX1801: Import stock media_profiles_vendor config
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -37,6 +37,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.settings.xml=/system/etc/media_profiles_vendor.xml
+
 # Netflix
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.netflix.bsp_rev=Q660-13149-1
