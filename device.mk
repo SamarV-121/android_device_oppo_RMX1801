7995839110b0d84801cfc000a0ce8a1571aecc8a
SamarV-121 <samarvispute121@gmail.com>
2020-03-09 17:25:32 +0530

    RMX1801: usb: Convert to RMX1801 targets
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.oppo_RMX1801
 
 # WiFi
 PRODUCT_COPY_FILES += \
